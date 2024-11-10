b83b88f1e05e7ebea501d20881f0751d9f946f9f
li lin <prohect@foxmail.com>
2024-11-10 22:29:25 +0800

    Update LoginLogic.java

--- src/main/java/com/prohect/sqlFrontend/main/login/LoginLogic.java
+++ src/main/java/com/prohect/sqlFrontend/main/login/LoginLogic.java
@@ -80,7 +80,7 @@ public class LoginLogic implements Initializable {
                 char[] chars = newValue.substring(oldValue.length()).toCharArray();
                 StringBuilder stringBuilder = new StringBuilder(oldValue);
                 for (char c : chars) {
-                    if (!Character.isLetter(c) && Character.isDigit(c)) continue;
+                    if (!Character.isLetter(c) && !Character.isDigit(c)) continue;
                     stringBuilder.append(c);
                 }
                 usernameField.setText(stringBuilder.toString());
