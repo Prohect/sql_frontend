38de67ce2c25304159a7f246d62cf2d6b6e2fa2c
li lin <prohect@foxmail.com>
2024-11-15 23:31:58 +0800

    concurrent update for Logger

--- sql_frontend_common/src/main/java/com/prohect/sqlFrontendCommon/Logger.java
+++ sql_frontend_common/src/main/java/com/prohect/sqlFrontendCommon/Logger.java
@@ -7,13 +7,16 @@ import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 @SuppressWarnings("unused")
 public class Logger {
-    public static final String logFileSuffix = MessageFormat.format("{0}.log", datetime());
-    public static final File logRoot = new File("log");
+    private static final String logFileSuffix = MessageFormat.format("{0}.log", datetime());
+    private static final File logRoot = new File("log");
     private static final String logFileName = "Log-";
     public static Logger logger = new Logger("basic");
+    private static final ScheduledExecutorService logExecutorService = new ScheduledThreadPoolExecutor(1);
 
     static {
         if (!logRoot.exists()) {
@@ -21,17 +24,15 @@ public class Logger {
         }
     }
 
-    public final File file;
-    public final String logFilePrefix;
+    private final File file;
     private boolean initialized = false;
 
     public Logger(String logFilePrefix) {
-        this.logFilePrefix = logFilePrefix;
         file = new File(logRoot, logFilePrefix + logFileName + logFileSuffix);
         logger = this;
     }
 
-    public static String datetime() {
+    private static String datetime() {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmmss"));
     }
 
@@ -63,45 +64,46 @@ public class Logger {
     }
 
     public void log2fileLn(String message) {
-        initialize();
-        try (RandomAccessFile raf = new RandomAccessFile(file, "rw"); FileChannel fc = raf.getChannel()) {
-            raf.seek(raf.length());
-            @SuppressWarnings("unused") int write = fc.write(ByteBuffer.wrap(processMessage(message + "\r\n").getBytes()));
-            fc.force(false);
-        } catch (Exception e) {
-            System.err.println("Error logging exception: " + e.getMessage());
-        }
+        logExecutorService.schedule(() -> {
+            initialize();
+            try (RandomAccessFile raf = new RandomAccessFile(file, "rw"); FileChannel fc = raf.getChannel()) {
+                raf.seek(raf.length());
+                @SuppressWarnings("unused") int write = fc.write(ByteBuffer.wrap(processMessage(message + "\r\n").getBytes()));
+                fc.force(false);
+            } catch (Exception e) {
+                System.err.println("Error logging exception: " + e.getMessage());
+            }
+        }, 0, java.util.concurrent.TimeUnit.MILLISECONDS);
     }
 
-    public void log2fileLnConcurrent(List<String> messages) {
-        new Thread(() -> log2fileLn(messages)).start();
-    }
 
     public void log2fileLn(List<String> messages) {
-        initialize();
-        try (RandomAccessFile raf = new RandomAccessFile(file, "rw"); FileChannel fc = raf.getChannel()) {
-            raf.seek(raf.length());
-            for (String message : messages) {
-                @SuppressWarnings("unused") int write = fc.write(ByteBuffer.wrap(processMessage(processMessage(message) + "\r\n").getBytes()));
+        logExecutorService.schedule(() -> {
+            initialize();
+            try (RandomAccessFile raf = new RandomAccessFile(file, "rw"); FileChannel fc = raf.getChannel()) {
+                raf.seek(raf.length());
+                for (String message : messages) {
+                    @SuppressWarnings("unused") int write = fc.write(ByteBuffer.wrap(processMessage(processMessage(message) + "\r\n").getBytes()));
+                }
+                fc.force(false);
+            } catch (Exception e) {
+                System.err.println("Error logging exception: " + e.getMessage());
             }
-            fc.force(false);
-        } catch (Exception e) {
-            System.err.println("Error logging exception: " + e.getMessage());
-        }
-    }
-
-    public void log2consoleLnConcurrent(List<String> messages) {
-        new Thread(() -> log2consoleLn(messages)).start();
+        }, 0, java.util.concurrent.TimeUnit.MILLISECONDS);
     }
 
     public void log2consoleLn(List<String> messages) {
-        initialize();
-        for (String s : messages) log2consoleLn(processMessage(s));
+        logExecutorService.schedule(() -> {
+            initialize();
+            for (String message : messages) System.out.println(message);
+        }, 0, java.util.concurrent.TimeUnit.MILLISECONDS);
     }
 
     public void log2consoleLn(String message) {
-        initialize();
-        System.out.println(message);
+        logExecutorService.schedule(() -> {
+            initialize();
+            System.out.println(message);
+        }, 0, java.util.concurrent.TimeUnit.MILLISECONDS);
     }
 
     public void log(Throwable e) {
